19ad53ba77d872abe06ac3896f83b964f0e7db1d
Olivia <166169277+oliviaalmseger@users.noreply.github.com>
2025-06-04 15:19:54 +0200

    style: update component TodoList to use Tailwind

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -27,7 +27,7 @@ export const AddTodo = ({addTodo}: AddTodoProps) => {
             <label htmlFor="todo-input" className="font-[Karla] text-2xl mb-16 text-[#004aad] font-bold">Ny att göra-post: </label>
             <div className="flex flex-col items-center gap-2 bg-white">
                 <input type="text" id="todo-input" value={addContent} onChange={handleChange} placeholder="Vad ska göras?" className="w-full p-1.5 mt-2.5 border-2 border-[#004aad] rounded font-[Coming_Soon] focus:border-[3px] focus:border-black outline-none" required />
-                <button className="bg-[#004aad] text-white px-4 py-2 mt-1.5 text-s border border-[#004aad] rounded hover:bg-[#3a69a7] transition-colors duration-200">Lägg till</button>
+                <button className="bg-[#004aad] text-white px-4 py-2 mt-1.5 text-s border border-[#004aad] rounded hover:bg-[#3a69a7] transition-colors duration-200 font-[Karla]">Lägg till</button>
             </div>
         </form>
     </div>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -9,14 +9,17 @@ type TodoListProps = {
 export const TodoList = ({todos, handleClick, handleDelete}: TodoListProps) => {
     return(
     <>
-    <ul>
+    <ul className="list-none p-0 m-0">
         {todos.map((todo => (
-            <li key={todo.id}>
-                <label className={todo.isDone ? 'done' : ''} >
-                    <input type="checkbox" checked={todo.isDone} onChange={() => handleClick(todo.id)} className="mr-2.5"/>
+            <li key={todo.id} className="font-[Coming_soon] flex items-center justify-between w-full py-2">
+                <label className={`${todo.isDone ? 'line-through opacity-60' : ''}`}>
+                    <input type="checkbox" checked={todo.isDone} onChange={() => handleClick(todo.id)} className="mr-2.5  w-4 h-4 
+                    border-2 border-[#004aad] rounded cursor-pointer appearance-none relative 
+                    checked:after:content-['✓'] checked:after:text-[#004aad] checked:after:absolute checked:after:top-[-4px] checked:after:left-[2px] 
+                    hover:ring-1 hover:ring-[#004aad] align-middle"/>
                     {todo.todoText} - {new Date(todo.date).toLocaleDateString()}
                 </label>
-                <button className="font-karla ml-auto px-3 py-1 text-sm border border-[#004aad] rounded text-[#004aad] hover:bg-[#f1f4fd] transition-colors duration-200 " onClick={() => handleDelete(todo.id)}>Ta bort</button>
+                <button className="font-[Karla] ml-auto px-3 py-1 text-sm border border-[#004aad] rounded text-[#004aad] hover:bg-[#f1f4fd] transition-colors duration-200 whitespace-nowrap" onClick={() => handleDelete(todo.id)}>Ta bort</button>
             </li>
         )))}
     </ul>
